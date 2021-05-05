f98c74fc9dec32dafb1017b532e716501ee53e05
CunningLobster <62791645+CunningLobster@users.noreply.github.com>
2021-05-05 08:04:31 +0300

    96

---
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace RPG.Saving
+{
+    public class SaveableEntity : MonoBehaviour
+    {
+        public string GetUniqueIdentifier()
+        {
+            return "";
+        }
+
+        public object CaptureState()
+        {
+            print("Capturing State " + GetUniqueIdentifier());
+            return null;
+        }
+
+        public void RestoreState(object state)
+        {
+            print("Restoring from State " + GetUniqueIdentifier());
+        }
+    }
+}
--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -3,27 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace RPG.Saving
 {
     public class SavingSystem : MonoBehaviour
     {
+        public object BynaryFormatter { get; private set; }
+
         public void Save(string savingFile)
         {
             string path = GetPathFromSaveFile(savingFile);
             print("Saved to: " + path); ;
             using (FileStream stream = File.Open(path, FileMode.Create))
             {
-                Transform playerTransform = GetPlayerTransform();
-                byte[] buffer = SerializeVector(playerTransform.position);
-                stream.Write(buffer, 0, buffer.Length);
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, CaptureState());
             }
-            
-        }
 
+        }
 
         public void Load(string savingFile)
         {
@@ -31,10 +29,8 @@ namespace RPG.Saving
             print("Load from: " + GetPathFromSaveFile(savingFile));
             using (FileStream stream = File.Open(path, FileMode.Open))
             {
-                byte[] buffer = new byte[stream.Length];
-                stream.Read(buffer, 0, buffer.Length);
-                Transform playerTransform = GetPlayerTransform();
-                playerTransform.position = DeserializeVector(buffer);
+                BinaryFormatter formatter = new BinaryFormatter();
+                RestoreState(formatter.Deserialize(stream));
             }
         }
 
@@ -43,28 +39,23 @@ namespace RPG.Saving
             return Path.Combine(Application.persistentDataPath, savingFile + ".sav");
         }
 
-        private Transform GetPlayerTransform()
+        private object CaptureState()
         {
-            return GameObject.FindGameObjectWithTag("Player").transform;
+            Dictionary<string, object> state = new Dictionary<string, object>();
+            foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            {
+                state[saveable.GetUniqueIdentifier()] = saveable.CaptureState();
+            }
+            return state;
         }
 
-        byte[] SerializeVector(Vector3 vector)
+        private void RestoreState(object state)
         {
-            byte[] vectorBytes = new byte[3 * 4];
-            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
-            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
-            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
-            return vectorBytes;
+            Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
+            foreach (SaveableEntity saveable in FindObjectsOfType<SaveableEntity>())
+            {
+                 saveable.RestoreState(stateDict[saveable.GetUniqueIdentifier()]);
+            }
         }
-
-        Vector3 DeserializeVector(byte[] buffer)
-        {
-            Vector3 result = new Vector3();
-            result.x = BitConverter.ToSingle(buffer, 0);
-            result.y = BitConverter.ToSingle(buffer, 4);
-            result.z = BitConverter.ToSingle(buffer, 8);
-            return result;
-        }
-
     }
 }
